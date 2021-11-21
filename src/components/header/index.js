1c54844b7a001e2644797415cd9440f63e3929f2
Matthew <plummerfernandez@gmail.com>
2021-11-21 22:28:58 +0000

    header mod to show collaborator name

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -63,9 +63,40 @@ export const Header = () => {
 
   return (
     <>
-      <header className={styles.container}>
+      // <header className={styles.container}>
+      //   <div className={styles.content}>
+      //     <a href='/'>
+      //       <div className={styles.logo}>
+      //         {/* HIC LOGO */}
+      //         {false && (
+      //           <svg viewBox="0 0 196.87 53.23" fill={'var(--text-color)'}>
+      //             <path
+      //               d="M228.9,79.31H211.51a2.26,2.26,0,0,1-.35-.34.75.75,0,0,1-.16-.42c0-11.42,0-22.85,0-34.43H193.24v35H175.41V26.27H228.9Z"
+      //               transform="translate(-32.03 -26.27)"
+      //             />
+      //             <path
+      //               d="M67.74,43.78V26.42H85.41V79.19H67.91V62.38a4.24,4.24,0,0,0-.52-.57.77.77,0,0,0-.42-.17H50V79.08H32V26.48H49.78v17.3Z"
+      //               transform="translate(-32.03 -26.27)"
+      //             />
+      //             <path
+      //               d="M103.62,43.79V26.43h53.6c.09,5.62,0,11.41.05,17.36Z"
+      //               transform="translate(-32.03 -26.27)"
+      //             />
+      //             <path
+      //               d="M103.71,61.71h53.38V78.84c-4.05.69-38.16.91-53.38.31Z"
+      //               transform="translate(-32.03 -26.27)"
+      //             />
+      //           </svg>
+      //         )}
+      //         {/* PRIDE LOGO */}
+      //         {false && <img src="/hen-pride.gif" alt="pride 2021" />}
+      //       </div>
+      //          HECTIC NUN
+      //     </a>
+
+       <header className={styles.container}>
         <div className={styles.content}>
-          <a href='/'>
+          <Button onClick={() => handleRoute('/')}>
             <div className={styles.logo}>
               {/* HIC LOGO */}
               {false && (
@@ -91,8 +122,31 @@ export const Header = () => {
               {/* PRIDE LOGO */}
               {false && <img src="/hen-pride.gif" alt="pride 2021" />}
             </div>
-               HECTIC NUN
-          </a>
+            HECTIC NUN
+          </Button>
+
+          <div className={styles.right}>
+            {!context.collapsed && context.proxyAddress && (
+              <div className={styles.mr}>
+                <Button onClick={() => context.setProxyAddress(null)} secondary>
+                  <Primary>exit collab</Primary>
+                </Button>
+              </div>
+            )}
+
+            <Button onClick={handleSyncUnsync} secondary>
+              <Primary>{button}</Primary> {/* Main address display here */}
+            </Button>
+
+            <Button onClick={context.toogleNavbar} secondary>
+              <VisuallyHidden>
+                {`${context.collapsed ? 'show' : 'hide'} menu`}
+              </VisuallyHidden>
+              <Menu isOpen={!context.collapsed} />
+            </Button>
+          </div>
+        </div>
+      </header>
 
           <div className={styles.right}>
             <Button onClick={handleSyncUnsync} secondary>
